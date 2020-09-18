0fbbfe4c1cdfc994c8581db5983004b09dbb30dc
Dana Jomar <jod@pop-os.localdomain>
2020-09-18 13:08:45 +0200

    change to first release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyALE",  
-    version="0.1.1",
+    version="1.0.0",
     author="Dana Jomar",
     author_email="dana.jomar@outlook.com",
     description="ALE plots with python",
