576339ea67f549d5a29e5016e21f9f4412f0b084
Dana Jomar <jod@pop-os.localdomain>
2021-08-11 13:20:40 +0200

    increase version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyALE",
-    version="1.1.1",
+    version="1.1.2",
     author="Dana Jomar",
     author_email="dana.jomar@outlook.com",
     description="ALE plots with python",
